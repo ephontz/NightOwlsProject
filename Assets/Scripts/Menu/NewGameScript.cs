525a793693e11ab793cfe2e1a094960f7cebd153
Walter Kautsky <Walter.Kautsky@fullsail.edu>
2015-09-15 17:25:19 -0400

    Main menu skeleton created.

---
+++ Assets/Scripts/Menu/NewGameScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NewGameScript : MonoBehaviour {
+
+
+	public AudioClip sound;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	public void LoadNewGame()
+	{
+		GetComponent<AudioSource>().PlayOneShot(sound);
+		Application.LoadLevel("tutorial");
+	}
+}
---
+++ Assets/Scripts/Menu/OptionsScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionsScript : MonoBehaviour {
+
+	public AudioClip sound;
+
+	
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+
+	//The function that's called when clicked.
+	public void OptionsMenu()
+	{
+		GetComponent<AudioSource>().PlayOneShot(sound);
+		Application.LoadLevel("Options Menu");
+	}
+}
