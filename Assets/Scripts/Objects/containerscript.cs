b514e8e9af4ef80ef4222ee17e131779f4f0ae9f
Eric <ephontz@fullsail.edu>
2015-09-15 17:38:14 -0400

    Use and Invis done

--- Assets/Scripts/Objects/containerscript.cs
+++ Assets/Scripts/Objects/containerscript.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class containerscript : MonoBehaviour {
 
+
+	float normDelay = 1.0f;
+	float delay = 1.0f;
+	int value = 10;
+	GameObject loot = null;
 	// Use this for initialization
-	void Start () {
-	
+	public void SetUp (float _delay, int _value, GameObject _loot) 
+	{
+		delay = normDelay = _delay;
+		value = _value;
+		loot = _loot;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	public float inUse () 
+	{
+		delay -= Time.deltaTime;
+		return delay;
 	}
+
+	public int Payout()
+	{
+		int temp = value;
+		value = 0;
+		return temp;
+	}
+
+	void OnCollisionExit2D(Collision2D col)
+	{
+		delay = normDelay;
+	}
+
 }
--- Assets/Scripts/Players/Invisiblilityscript.cs
+++ Assets/Scripts/Players/Invisiblilityscript.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum lightLevel {invisible = 0, dark =1, shadow = 2, normal = 3, full = 4, bright = 5}
+
 public class Invisiblilityscript : MonoBehaviour {
+
+	int curLight = 3;
+	bool invisActive = false;
+	bool onCooldown = false;
+	float duration = 3;
+	float fullDuration = 3;
+	float cooldown = 30;
+	float fullCooldown = 30;
 
 	// Use this for initialization
 	void Start () {
@@ -9,7 +19,48 @@ public class Invisiblilityscript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (invisActive) 
+		{
+			duration -= Time.deltaTime;
+
+			if(duration <= 0)
+			{
+				invisActive = false;
+				duration = fullDuration;
+				onCooldown = true;
+				curLight += 1;
+			}
+		}
+
+		if (onCooldown) 
+		{
+			cooldown -= Time.deltaTime;
+
+			if(cooldown <= 0)
+			{
+				cooldown = fullCooldown;
+				onCooldown = false;
+			}
+		}
+
+	}
+
+	public void Invisibility()
+	{
+		if (!onCooldown) 
+		{
+			invisActive = true;
+			curLight -= 1;
+			if (curLight < 0)
+				curLight = 0;
+		}
+
+	}
+
+	public int VisCheck()
+	{
+		return curLight;
 	}
 }
