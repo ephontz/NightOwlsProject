a18d14ea24c899a0d5e1a7ac2a507e049a5b281c
Walter Kautsky <Walter.Kautsky@fullsail.edu>
2015-09-17 13:17:38 -0400

    Main camera created and camera script created so it follows player.

---
+++ Assets/Scripts/Menu/OptionsButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsButtonScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void Back()
+	{
+		Application.LoadLevel (0);
+	}
+
+	public void Fullscreen()
+	{
+		Screen.fullScreen = true;
+	}
+
+	public void Windowed()
+	{
+		Screen.fullScreen = false;
+	}
+
+
+}
