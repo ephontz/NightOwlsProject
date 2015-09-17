854217b53a13fada249a35348550e6676d3a91a0
Walter Kautsky <Walter.Kautsky@fullsail.edu>
2015-09-17 18:30:08 -0400

    Created the credits menu option in the main menu.

---
+++ Assets/Scripts/Menu/CreditsButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsButtonScript : MonoBehaviour {
+
+	public AudioClip sound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	public void LoadCreditsMenu()
+	{
+		GetComponent<AudioSource>().PlayOneShot(sound);
+		Application.LoadLevel("Credits Menu");
+	}
+}
